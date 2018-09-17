0958a293f619dad30c189949cae41faeeab34d77
Kazuki Kishishita <kishi-tennis0310@rakuten.jp>
2018-09-17 14:28:16 +0800

    UI部品の設置

---
+++ RealmTodo/AddViewController.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func tapBtn(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+}
